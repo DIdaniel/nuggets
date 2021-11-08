f508fc6e4634e1af61997cbab345ad900c603ed4
KIM DONG IL <cappu159@gmail.com>
2021-11-08 22:07:24 +0900

    Dynamic Object keys

--- 03-dynamic-object-keys/app.js
+++ 03-dynamic-object-keys/app.js
@@ -1,13 +1,54 @@
-const person = ['hello', 'great'];
+const person = {
+  name: "John",
+};
 
-const testPerson = person.map(function (item) {
-  console.log(item);
-});
+console.log(person.name);
 
-const newPerson = person.map(function (item) {
-  if (item === 'hello') {
-    return item.toUpperCase();
-  }
-  return item;
-});
-console.log(newPerson);
+person.age = 25;
+
+console.log(person);
+
+//
+
+const items = {
+  "featured-items": ["item1", "item2"],
+};
+
+console.log(items["featured-items"]);
+console.log(person["name"]);
+
+let appState = "loading";
+const keyName = "Computer";
+const app = {
+  [appState]: true,
+};
+
+app[keyName] = "APPLE";
+console.log(app);
+
+const state = {
+  loading: true,
+  name: "",
+  job: '"',
+};
+
+function updated(key, value) {
+  state[key] = value;
+}
+
+updated("name", "Patrick");
+updated("job", "Developer");
+console.log(state);
+// const person = ['hello', 'great'];
+
+// const testPerson = person.map(function (item) {
+//   console.log(item);
+// });
+
+// const newPerson = person.map(function (item) {
+//   if (item === 'hello') {
+//     return item.toUpperCase();
+//   }
+//   return item;
+// });
+// console.log(newPerson);
