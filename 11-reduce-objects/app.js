44e979d177f4fb008a1fe8875a0efd426141a153
KIM DONG IL <cappu159@gmail.com>
2021-11-08 22:52:53 +0900

    reduce higher

--- 11-reduce-objects/app.js
+++ 11-reduce-objects/app.js
@@ -24,5 +24,41 @@ const cart = [
   },
 ];
 
+let { totalItems, cartTotal } = cart.reduce(
+  (total, cur) => {
+    const { amount, price } = cur;
+
+    // count item
+    total.totalItems += amount;
+    total.cartTotal += price * amount;
+
+    return total;
+  },
+  {
+    totalItems: 0,
+    cartTotal: 0,
+  }
+);
+
+cartTotal = parseFloat(cartTotal.toFixed(2));
+console.log(totalItems, cartTotal);
 // Github repo example
 const url = "https://api.github.com/users/john-smilga/repos?per_page=100";
+
+const fetchRepos = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  const newData = data.reduce((total, repo) => {
+    const { language } = repo;
+    if (total[language]) {
+      total[language] = total[language] + 1;
+    } else {
+      total[language] = 1;
+    }
+    return total;
+  }, {});
+
+  console.log(newData);
+};
+
+fetchRepos();
